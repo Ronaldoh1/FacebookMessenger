c46dc19a78388e23e92c795242e86f1f73ccb5a7
Ronald Hernandez <2ronald.hernandez@gmail.com>
2016-12-30 03:02:17 -0500

    added models

---
+++ FacebookMessenger/Friend.swift
@@ -0,0 +1,16 @@
+//
+//  Friend.swift
+//  FacebookMessenger
+//
+//  Created by Ronald Hernandez on 12/30/16.
+//  Copyright © 2016 Ronaldoh1. All rights reserved.
+//
+
+import UIKit
+
+class Friend: NSObject {
+
+    var name: String?
+    var profileImageName: String?
+    
+}
---
+++ FacebookMessenger/Message.swift
@@ -0,0 +1,18 @@
+//
+//  Message.swift
+//  FacebookMessenger
+//
+//  Created by Ronald Hernandez on 12/30/16.
+//  Copyright © 2016 Ronaldoh1. All rights reserved.
+//
+
+import UIKit
+
+class Message: NSObject {
+
+    var text: String?
+    var date: NSDate?
+
+    var friend: Friend?
+    
+}
